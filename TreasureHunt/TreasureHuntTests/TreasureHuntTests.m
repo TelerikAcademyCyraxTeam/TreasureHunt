3acb6e6ba14ad27e3084b5a89a38ed59c6c81182
Admin <admin@Mac-Admin-2.local>
2014-11-02 16:37:34 +0300

    Initial commit

---
+++ TreasureHunt/TreasureHuntTests/TreasureHuntTests.m
@@ -0,0 +1,40 @@
+//
+//  TreasureHuntTests.m
+//  TreasureHuntTests
+//
+//  Created by Admin on 11/2/14.
+//  Copyright (c) 2014 Cyrax. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface TreasureHuntTests : XCTestCase
+
+@end
+
+@implementation TreasureHuntTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
