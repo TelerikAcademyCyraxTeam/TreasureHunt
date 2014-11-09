1844ceb8e715215b9b6fb95bdbfaf69b5148446f
Sophia Lazarova <sophia.lazarova@yahoo.com>
2014-11-09 18:57:26 +0300

    camera view

---
+++ TreasureHunt/TreasureHunt/AddPhotoViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddPhotoViewController.h
+//  TreasureHunt
+//
+//  Created by Admin on 11/9/14.
+//  Copyright (c) 2014 Cyrax. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddPhotoViewController : UIViewController
+
+@end
